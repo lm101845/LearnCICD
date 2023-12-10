96e7c55c3bc35b7ee072b29efbb30ac29491fb73
lm101845 <1018457683@qq.com>
2023-12-10 15:43:27 +0800

    提交

---
+++ 06-Shell脚本/02-自己常用脚本/explorer.bat
@@ -0,0 +1,13 @@
+@echo off
+
+start www.baidu.com
+
+start https://gitee.com/mengqingshu/lfbank_ald_app
+
+start https://chat.openai.com/chat
+
+start http://localhost:9102/#/
+
+start https://docs.qq.com/sheet/DU2tHdEpMaWZHeGZ5?tab=BB08J2
+
+pause
